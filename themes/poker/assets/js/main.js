dd85101fe659c7085768e6408321fdb693fa028e
niazmorshad <78914574+niazmorshad@users.noreply.github.com>
2021-12-06 00:32:19 +0600

    ,

--- themes/poker/assets/js/main.js
+++ themes/poker/assets/js/main.js
@@ -172,4 +172,68 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
     new WOW().init();
 
+
+
+    if( $('.newsSlider').length ){
+    $('.newsSlider').slick({
+      dots:false,
+      infinite: false,
+      autoplay: false,
+      autoplaySpeed: 2000,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      prevArrow: $('.fl-prev-next .fl-prev'),
+      nextArrow: $('.fl-prev-next .fl-next'),
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
+
+  function attrvaluupdate(){
+
+    var windowsize =  $(window).width();
+    console.log(windowsize);
+    var containerwidth = $(".container").width();
+
+    var marginleft = ((windowsize-containerwidth)/2);
+
+    $('.news-sec-cntlr').css({
+      "padding-left":marginleft,
+    });
+    $('.fl-prev-next').css({
+      "right":marginleft,
+    })
+  }
+  attrvaluupdate();
+  $(window).resize(function(){
+      attrvaluupdate();
+  });
+
 })(jQuery);
